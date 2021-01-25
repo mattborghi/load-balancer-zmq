e29d92be97a2ea6e4df4028e2b76bac9b884a4eb
Matias Borghi <borghi.matias@gmail.com>
2021-01-25 12:08:48 -0300

    minor improvement to sink: add docs + show results

--- LoadBalancer/sink.py
+++ LoadBalancer/sink.py
@@ -1,4 +1,5 @@
 from multiprocessing import Event
+from time import sleep
 import zmq
 
 SINK_HOST = "127.0.0.1"
@@ -6,6 +7,21 @@ SINK_PORT = 5758
 
 
 class Sink(object):
+    """
+    Pull work from the proxy of the form of the job object class 
+    {'id': xxx, 'name': 'xxx', 'result': xxx, 'number1': xxx, 'number2': xxx}.
+
+
+    Parameters
+    ----------
+        event : Event
+            Event object of multiprocessing used for terminating the processes.
+        host: str
+            Worker host connection
+        port: int
+            Worker port connection
+
+    """
     def __init__(
         self, event: Event = Event(), host: str = SINK_HOST, port: int = SINK_PORT
     ):
@@ -28,6 +44,12 @@ class Sink(object):
         # is this really necessary?
         exit()
 
+    def _before_finishing(self):
+        sleep(1)
+        print("\n\n")
+        print("Results processed in Sink")
+        print(self.results)
+
     def _run(self):
         try:
             while not self.stop_event.is_set():
@@ -40,6 +62,7 @@ class Sink(object):
         except Exception as e:
             print(e)
         finally:
+            self._before_finishing()
             self._disconnect()
 
 
